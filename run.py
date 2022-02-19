4c19d729c519075d917b53827922b583746e67e7
smile31768 <60568280+smile31768@users.noreply.github.com>
2022-02-19 16:10:22 +0800

    Update run.py

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ def csv2xlsx(csvpath,xlsxpath):
     csv.to_excel(xlsxpath, sheet_name='sheet1',index=False)
 
 # main
+fail_count=0
 csv_head=['姓名','民族','住址','身份证号','生日','性别']
 create_csv(csv_head)            #创建csv文档
 print('开始识别身份证')
@@ -21,8 +22,12 @@ for i in findAllFile('img'):    #遍历img文件夹，识别身份证信息
     idinfo=idocr(i)
     if not idinfo==[]:
         print(idinfo)
+    else:
+        fail_count+=1
     write_csv(idinfo)
     time.sleep(0.5)
 print('正在写入xlsx文件')
 csv2xlsx('output.csv','output.xlsx')
 os.remove('output.csv')
+print(f'识别完成，一共{fail_count}个识别失败')
+time.sleep(2)
